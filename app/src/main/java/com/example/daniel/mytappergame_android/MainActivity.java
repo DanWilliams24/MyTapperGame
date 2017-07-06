212a8f2f12fc1790b58636cbb255c60d08cf45f0
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-06 19:19:53 -0400

    testing

---
+++ app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.daniel.mytappergame_android;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("Hello World!!");
+    }
+}
