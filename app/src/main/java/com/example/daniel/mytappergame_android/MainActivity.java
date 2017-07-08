20ab37ff23f2bed9902716d15ce35e998268cd71
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-07 23:26:09 -0400

    Added Timer

--- app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
+++ app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.daniel.mytappergame_android;
 
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.CountDownTimer;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     Integer score = 0;
-
-
+    Integer time = 60;
+    Integer delay = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView scoreLabel = (TextView) findViewById(R.id.textView2);
+        //Code to be run after the scene is loaded
+        TextView scoreLabel = (TextView) findViewById(R.id.textView2);//Resets score label
         scoreLabel.setText("Score: " + score);
 
         Button button = (Button) findViewById(R.id.button);
@@ -27,12 +30,30 @@ public class MainActivity extends AppCompatActivity {
                 addScore();
             }
         });
+
+        new CountDownTimer(60000, 1000){ //New Timer of duration 60 seconds, running every second(1000milliseconds)
+            public void onTick(long millisUntilFinished) {
+                TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+                timeLabel.setText("Time: " + millisUntilFinished / 1000);
+                time -= 1;
+            }
+
+            public void onFinish() {
+                TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+                timeLabel.setText("Timer Done!");
+            }
+        }.start();
+
     }
 
     private void addScore() {
-        score += 1;
-        System.out.print("Added one to the score");
-        TextView scoreLabel = (TextView) findViewById(R.id.textView2);
-        scoreLabel.setText("Score: " + score);
+        if(time > 0){
+            score += 1;
+            System.out.print("Added one to the score");
+            TextView scoreLabel = (TextView) findViewById(R.id.textView2);
+            scoreLabel.setText("Score: " + score);
+        }
     }
+
+
 }
