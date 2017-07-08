edef501ec03177dfc4f352efc44cb9541f091069
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-07 22:06:55 -0400

    Allows User to tap a button, and points are added to the score

--- app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
+++ app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
@@ -3,16 +3,36 @@ package com.example.daniel.mytappergame_android;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Integer score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("Hello World!!");
+        TextView scoreLabel = (TextView) findViewById(R.id.textView2);
+        scoreLabel.setText("Score: " + score);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                addScore();
+            }
+        });
     }
-    
+
+    private void addScore() {
+        score += 1;
+        System.out.print("Added one to the score");
+        TextView scoreLabel = (TextView) findViewById(R.id.textView2);
+        scoreLabel.setText("Score: " + score);
+    }
 }
