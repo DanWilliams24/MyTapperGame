af0aa3a6e24a23959094ebf49c366ad7c031b17d
DanWilliams24 <01danielwilliams@gmail.com>
2017-07-09 17:33:59 -0400

    Fixed countdown timer.

--- app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
+++ app/src/main/java/com/example/daniel/mytappergame_android/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.CountDownTimer;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -19,17 +20,51 @@ public class MainActivity extends AppCompatActivity {
     public static Integer score = 0;
     public static Integer time = 60;
     gameState currentState;
-
+    boolean gameDidStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Code to be run after the scene is loaded
-
-
+        gameDidStart = false;
+        final TextView directionsLabel = (TextView) findViewById(R.id.directionsLabel);
+        final TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+        final Button tapMeButton = (Button) findViewById(R.id.tapMeButton);
+        final TextView scoreLabel = (TextView)findViewById(R.id.scoreLabel);
+        final TextView tapToStartLabel = (TextView)findViewById(R.id.tapToStartLabel);
         gameSetup();
+
+
         uiSetup();
+        RelativeLayout gameView = (RelativeLayout) findViewById(R.id.activity_main);
+        tapToStartLabel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                directionsLabel.setVisibility(View.VISIBLE);
+                timeLabel.setVisibility(View.VISIBLE);
+                tapMeButton.setVisibility(View.VISIBLE);
+                scoreLabel.setVisibility(View.VISIBLE);
+                tapToStartLabel.setVisibility(View.INVISIBLE);
+                tapToStartLabel.setEnabled(false);
+
+                new CountDownTimer(60000, 1000){ //New Timer of duration 60 seconds, running every second(1000milliseconds)
+                    public void onTick(long millisUntilFinished) {
+                        TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+                        timeLabel.setText("Time: " + millisUntilFinished / 1000);
+                        time -= 1;
+                    }
+
+                    public void onFinish() {
+                        TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+                        timeLabel.setText("Timer Done!");
+                        moveToView();
+                    }
+                }.start();
+
+            }
+
+        });
 
 
 
@@ -54,47 +89,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void uiSetup() {
-        if(currentState == gameState.beforeGame){
-            final TextView directionsLabel = (TextView) findViewById(R.id.directionsLabel);
-            final TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
-            final Button tapMeButton = (Button) findViewById(R.id.tapMeButton);
-            final TextView scoreLabel = (TextView)findViewById(R.id.scoreLabel);
-            final TextView tapToStartLabel = (TextView)findViewById(R.id.tapToStartLabel);
 
-            directionsLabel.setVisibility(View.VISIBLE);
-            timeLabel.setVisibility(View.INVISIBLE);
-            tapMeButton.setVisibility(View.INVISIBLE);
-            scoreLabel.setVisibility(View.INVISIBLE);
-            tapToStartLabel.setVisibility(View.VISIBLE);
+        final TextView directionsLabel = (TextView) findViewById(R.id.directionsLabel);
+        final TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
+        final Button tapMeButton = (Button) findViewById(R.id.tapMeButton);
+        final TextView scoreLabel = (TextView)findViewById(R.id.scoreLabel);
+        final TextView tapToStartLabel = (TextView)findViewById(R.id.tapToStartLabel);
 
-            RelativeLayout gameView = (RelativeLayout) findViewById(R.id.activity_main);
-            gameView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
+        directionsLabel.setVisibility(View.VISIBLE);
+        timeLabel.setVisibility(View.INVISIBLE);
+        tapMeButton.setVisibility(View.INVISIBLE);
+        scoreLabel.setVisibility(View.INVISIBLE);
+        tapToStartLabel.setVisibility(View.VISIBLE);
+        tapToStartLabel.setEnabled(true);
 
-                    directionsLabel.setVisibility(View.VISIBLE);
-                    timeLabel.setVisibility(View.VISIBLE);
-                    tapMeButton.setVisibility(View.VISIBLE);
-                    scoreLabel.setVisibility(View.VISIBLE);
-                    tapToStartLabel.setVisibility(View.INVISIBLE);
-                    currentState = gameState.duringGame;
-                    new CountDownTimer(60000, 1000){ //New Timer of duration 60 seconds, running every second(1000milliseconds)
-                        public void onTick(long millisUntilFinished) {
-                            TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
-                            timeLabel.setText("Time: " + millisUntilFinished / 1000);
-                            time -= 1;
-                        }
+        RelativeLayout gameView = (RelativeLayout) findViewById(R.id.activity_main);
+        /*
+        gameView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
 
-                        public void onFinish() {
-                            TextView timeLabel = (TextView) findViewById(R.id.timeLabel);
-                            timeLabel.setText("Timer Done!");
-                            moveToView();
-                        }
-                    }.start();
+                directionsLabel.setVisibility(View.VISIBLE);
+                timeLabel.setVisibility(View.VISIBLE);
+                tapMeButton.setVisibility(View.VISIBLE);
+                scoreLabel.setVisibility(View.VISIBLE);
+                tapToStartLabel.setVisibility(View.INVISIBLE);
+                currentState = gameState.duringGame;
+                gameDidStart = true;
                 }
             });
+            */
+
         }
-    }
+
 
 
     private void addScore() {
